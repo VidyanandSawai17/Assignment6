7df46312f68d4f3c07f7e819b1edd528624509aa
VidyanandSawai17 <vidyanandsawai17@gmail.com>
2023-01-21 17:38:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   Create class Product → prod id, name, company & price
+                 Assign values in the constructor
+                 If price > 2000  give 15 % discount
+                 If price < 2000  give 5 % discount
+                 And display product details
+                 reate two objects of product class & display the details
+                                                                                */
+
+            // creating Object 
+
+            //create object of student class
+            Student stud = new Student(1, "headphone", "boAt", 1900);
+            stud.Discount();
+            Console.WriteLine(stud.Display());
+
+
+
+        }
+    }
+}
---
+++ discount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    public class Student // variables, constructor,method,property,indexer,event
+    {
+        // variables or data members
+        private int id;
+        private string name;
+        private string company;
+        private double price, discount;
+
+        //constructor
+        public Student()
+        {
+            id = 1;
+            name = "Test";
+            company = "xyz";
+            price = 0;
+
+
+        }
+        public Student(int id, string name, string company, double price)
+        {
+            this.id = id; // this keyword points to the data member
+            this.name = name;
+            this.company = company;
+            this.price = price;
+
+        }
+        public void Discount()
+        {
+            if (price > 2000)
+            {
+                discount = price * 0.15;
+            }
+            else if (price < 2000)
+            {
+                discount = price * 0.5;
+            }
+        }
+        public string Display()
+        {
+            return $"\n ID : {id} \n Name : {name} \n Company : {company} \n Price = {price} rs  \n Discount = {discount} rs";
+        }
+    }
+}
